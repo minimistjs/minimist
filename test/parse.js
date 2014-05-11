9bf2d36f1d3b8795be90b8f7de0a937f098aa394
James Halliday <mail@substack.net>
2014-05-11 23:39:27 +0200

    dedicated num test

--- test/parse.js
+++ test/parse.js
@@ -42,32 +42,6 @@ test('comprehensive', function (t) {
     t.end();
 });
 
-test('nums', function (t) {
-    var argv = parse([
-        '-x', '1234',
-        '-y', '5.67',
-        '-z', '1e7',
-        '-w', '10f',
-        '--hex', '0xdeadbeef',
-        '789'
-    ]);
-    t.deepEqual(argv, {
-        x : 1234,
-        y : 5.67,
-        z : 1e7,
-        w : '10f',
-        hex : 0xdeadbeef,
-        _ : [ 789 ]
-    });
-    t.deepEqual(typeof argv.x, 'number');
-    t.deepEqual(typeof argv.y, 'number');
-    t.deepEqual(typeof argv.z, 'number');
-    t.deepEqual(typeof argv.w, 'string');
-    t.deepEqual(typeof argv.hex, 'number');
-    t.deepEqual(typeof argv._[0], 'number');
-    t.end();
-});
-
 test('flag boolean', function (t) {
     var argv = parse([ '-t', 'moo' ], { boolean: 't' });
     t.deepEqual(argv, { t : true, _ : [ 'moo' ] });
