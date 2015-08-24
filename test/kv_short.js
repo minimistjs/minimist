63416b8cd1d0d70e4714564cce465a36e4dd26d7
James Halliday <mail@substack.net>
2015-08-24 08:48:44 -0500

    failing -k=v short test

---
+++ test/kv_short.js
@@ -0,0 +1,9 @@
+var parse = require('../');
+var test = require('tape');
+
+test('short -k=v' , function (t) {
+    t.plan(1);
+    
+    var argv = parse([ '-b=123' ]);
+    t.deepEqual(argv, { b: 123, _: [] });
+});
