f72ab7f4572adc52902c9b6873cc969192f01b10
James Halliday <mail@substack.net>
2015-08-24 08:53:37 -0500

    failing kv short test

--- test/kv_short.js
+++ test/kv_short.js
@@ -7,3 +7,10 @@ test('short -k=v' , function (t) {
     var argv = parse([ '-b=123' ]);
     t.deepEqual(argv, { b: 123, _: [] });
 });
+
+test('multi short -k=v' , function (t) {
+    t.plan(1);
+    
+    var argv = parse([ '-a=whatever -b=robots' ]);
+    t.deepEqual(argv, { a: 'whatever', b: 'robots', _: [] });
+});
