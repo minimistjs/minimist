72239e6f0ea77d8be0ad4f682b7ae7d142144395
Wes Carr <wescarr@gmail.com>
2022-10-26 11:09:20 -0700

    [Tests] Remove duplicate test (#12)
    Fixes #8

--- test/dash.js
+++ test/dash.js
@@ -19,9 +19,8 @@ test('-', function (t) {
 });
 
 test('-a -- b', function (t) {
-	t.plan(3);
+	t.plan(2);
 	t.deepEqual(parse(['-a', '--', 'b']), { a: true, _: ['b'] });
-	t.deepEqual(parse(['--a', '--', 'b']), { a: true, _: ['b'] });
 	t.deepEqual(parse(['--a', '--', 'b']), { a: true, _: ['b'] });
 });
 
